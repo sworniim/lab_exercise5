c5a6b141eceac26beb321935b2782bd9ad8234be
swornimmaharjan <sworniimmaharjan@gmail.com>
2021-03-18 13:09:38 +0545

    homeworkpush

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Task 2 (Lab-5)
+# Home Work (Lab-5)
 ### App screenshot
-![](./1.png)
-![](./2.png)
-![](./3.png)
+![](./managing.png)
--- app/src/main/java/com/example/twoactivity/MainActivity.java
+++ app/src/main/java/com/example/twoactivity/MainActivity.java
@@ -6,123 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    public static final String EXTRA_MESSAGE            = "com.example.android.activity_second.extra.MESSAGE";
-    public static final int TEXT_REQUEST = 1;
-
-    private EditText mMessageEditText;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
+    public static final String EXTRA_MESSAGE ="" ;
+    private TextView countTextView;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.d(LOG_TAG, "-------");
-        Log.d(LOG_TAG, "onCreate");
-
-        // Initialize all the view variables.
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
-
-        // Restore the saved state.
-        // See onSaveInstanceState() for what gets saved.
-        if (savedInstanceState != null) {
-            boolean isVisible =
-                    savedInstanceState.getBoolean("reply_visible");
-            // Show both the header and the message views. If isVisible is
-            // false or missing from the bundle, use the default layout.
-            if (isVisible) {
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(savedInstanceState
-                        .getString("reply_text"));
-                mReplyTextView.setVisibility(View.VISIBLE);
-            }
-        }
-
-    }
-
-
-    public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
-        Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivityForResult(intent, TEXT_REQUEST);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_TAG, "onStart");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(LOG_TAG, "onPause");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(LOG_TAG, "onRestart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(LOG_TAG, "onResume");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(LOG_TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(LOG_TAG, "onDestroy");
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
-            }
+        countTextView = findViewById(R.id.count_textView);
+        if (savedInstanceState != null){
+            String i = savedInstanceState.getString("count_value");
+            countTextView.setText(i);
+            count = Integer.parseInt(i);
         }
     }
+
+    public void increment(View view) {
+        count ++;
+        countTextView.setText(String.valueOf(count));
+    }
+
     @Override
-    public void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        // If the heading is visible, message needs to be saved.
-        // Otherwise we're still using default layout.
-        if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
-            outState.putBoolean("reply_visible", true);
-            outState.putString("reply_text",
-                    mReplyTextView.getText().toString());
-        }
+        outState.putString("count_value", countTextView.getText().toString());
     }
-
-
-
-
 
 }
